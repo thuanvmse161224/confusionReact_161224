70e9478c9aef53b1ff8a278a058781cb535487f2
kiriHahnchen <105254386+thuanvmse161224@users.noreply.github.com>
2022-06-23 11:27:16 +0700

    separate menu compo

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 class Menu extends Component {
+    renderMenuItem (dish, onClick) {
+        /* debug sec
+        console.log("in renderMenuItem");
+        console.log(dish);
+        console.log(dish.dish);
+        console.log(dish.onClick);
+        console.log(onClick);
+        console.log("in renderMenuItem ending");
+        */
+        const disha = dish.dish;
+        return (
+            <Card onClick={() => dish.onClick(disha.id)} >
+                <CardImg width="100%" src={disha.image} alt={disha.name} />
+                <CardImgOverlay>
+                    <CardTitle>{disha.name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        );
+        
+    }
+
     render() {
         const menu = this.props.dishes.map((dish) => {
+            /*
+            console.log("in menu render");
+            console.log(dish);
+            console.log(this.props.onClick);
+            */
             return (
                 <div className="col-12 col-md-5 m-1">
-                    <Card   key={dish.id}
-                            onClick={() => this.props.onClick(dish.id)}
-                    >
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{dish.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
+                    <this.renderMenuItem dish={dish} onClick={this.props.onClick} />
                 </div>
             );
         });
