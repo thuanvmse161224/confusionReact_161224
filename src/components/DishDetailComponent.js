66f85019d28d92790e4890dd415af3fb043fb81b
kiriHahnchen <105254386+thuanvmse161224@users.noreply.github.com>
2022-06-23 10:02:13 +0700

    revert to separate

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,36 +2,54 @@ import React, { Component } from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
 class DishDetail extends Component {
+    renderDish (inputDish) {
+        const dish = inputDish.inputDish; 
+        return(
+            <div className="col-12 col-sm-5">
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name}/>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+        
+    }
+
+    renderComments(comments) {        
+        const comment = comments.comments.map((item) => {
+            return (
+                <div>
+                    <p>{item.comment}</p>
+                    <p>{item.author} {item.date}</p>
+                </div>
+            );
+        });
+        
+        return (
+            <div className="col-12 col-sm-7">
+                <h2>comments</h2>
+                {comment}
+            </div>
+        );
+        
+    }
+
+
     render() {
         if (this.props.dish != null) {
             const dish = this.props.dish;
-            const comment = dish.comments.map((item) => {
-                return (
-                    <div>
-                        <p>{item.comment}</p>
-                        <p>{item.author} {item.date}</p>
-                    </div>
-                );
-            });
+            const comments = dish.comments;
 
             return (
                 <div className="container">
                 <div className="row">
-                    <div className="col-12 col-sm-5">
-                        <Card>
-                            <CardImg top src={dish.image} alt={dish.name}/>
-                            <CardBody>
-                                <CardTitle>{dish.name}</CardTitle>
-                                <CardText>{dish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div className="col-12 col-sm-7">
-                        <h2>comments</h2>
-                        {comment}
-                    </div>
+                    <this.renderDish inputDish={dish}/>
+                    <this.renderComments comments={comments} />
                 </div>
-            </div>
+                </div>
             )
         }
         else {
